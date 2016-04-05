65d04c83b6b01e9f49357168447ffe4ef0d1d628
jjborde <jayeshb@zabuzalabs.com>
2016-04-05 16:44:51 +0530

    tweleth commit

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+//var io = require('../..')(server);
 var port = process.env.PORT || 3000;
+
+
+// Routing
+app.use(express.static(__dirname + '/public'));
 
 /*server.listen(port, function () {
   console.log('Server listening at port %d', port);
